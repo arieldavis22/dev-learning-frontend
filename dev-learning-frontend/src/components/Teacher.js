3fce0c8406f04d1125800cfce92744000feab4b7
arieldavis22 <aadavisbusiness@gmail.com>
2020-06-05 19:48:32 -0400

    Teacher Hooks

--- dev-learning-frontend/src/components/Teacher.js
+++ dev-learning-frontend/src/components/Teacher.js
@@ -1,66 +1,50 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { allTeacherLessons } from '../services/users'
 import { Divider, Button } from 'semantic-ui-react'
 
-class Teacher extends Component {
-    state = {  
-        lessons: []
-    }
+const Teacher = (props) => {
+    const [lessons, setLessons] = useState([])
+    const { id, first_name, last_name, handleTeacherFollow, notFollowed, follow } = props
+    const menu = useSelector(state => state.app.menu)
+    const dispatch = useDispatch()
+    const setLessonID = lessonId => dispatch({type: "SET_LESSON_ID", lessonId})
 
-    componentDidMount() {
-        allTeacherLessons(this.props.id)
-        .then(data => {
-            this.setState({
-                lessons: data
-            })
-        })
-    }
+    useEffect(() => {
+        allTeacherLessons(props.id)
+        .then(data => setLessons(data))
+    })
 
-    handleID = (id) => {
-        this.props.setLessonID(id)
-    }
-
-    render() { 
-        const { id, first_name, last_name, handleTeacherFollow, notFollowed, follow } = this.props
-        return (  
+    const handleID = (id) => {
+            setLessonID(id)
+        }
+    return (  
             <div>
                 <h2>{first_name}, {last_name}</h2> 
                 {follow ? 
                 <div>
-                    {this.state.lessons.map(lesson => 
+                    {lessons.map(lesson => 
                     <div>
                         <h3>Title: {lesson.title}</h3>
                         <p>Description: {lesson.description}</p>
                         <NavLink to="/add-lesson" exact>
-                            {/* <button onClick={() => this.handleID(lesson.id)}>Add Lesson</button> */}
-                            <Button color={this.props.menu ? 'purple' : null} onClick={() => this.handleID(lesson.id)}>Add Lesson</Button>
+                            <Button color={menu ? 'purple' : null} onClick={() => handleID(lesson.id)}>Add Lesson</Button>
                         </NavLink>
-                    </div>
-                    )}
+                    </div>)}
+
                     <Divider/>
+
                 </div> 
-                : 
-                null}
+                : null}
+
                 {notFollowed ? 
                 <div>
-                    {/* <button onClick={() => handleTeacherFollow(id)}>Follow</button> */}
-                    <Button color={this.props.menu ? 'purple' : null} onClick={() => handleTeacherFollow(id)}>Follow</Button>
+                    <Button color={menu ? 'purple' : null} onClick={() => handleTeacherFollow(id)}>Follow</Button>
                     <Divider/>
                 </div> : null }
             </div>
-        );
-    }
+    );
 }
-const mapStateToProps = state => ({
-    lessonID: state.lesson.lessonID,
-    menu: state.app.menu
-})
 
-const mapDispatchToProps = dispatch => ({
-    setLessonID: lessonId => dispatch({type: "SET_LESSON_ID", lessonId})
-
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(Teacher);
+export default Teacher;
