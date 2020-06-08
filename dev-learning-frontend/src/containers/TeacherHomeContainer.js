5b7103654ae8bd37a36ed3dffb6bc5329beb2c94
arieldavis22 <aadavisbusiness@gmail.com>
2020-06-08 17:29:29 -0400

    teacher home hooks

--- dev-learning-frontend/src/containers/TeacherHomeContainer.js
+++ dev-learning-frontend/src/containers/TeacherHomeContainer.js
@@ -1,32 +1,25 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {NavLink} from 'react-router-dom'
 import { Button } from 'semantic-ui-react'
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
-
-
-class TeacherHomeContainer extends Component {
-    render() { 
-        return (  
-            <div>
-                <NavLink to="/classroom" exact>
-                    <Button color={this.props.menu ? 'purple' : null} icon='book' size='massive' content='Classrooms'/>
-                </NavLink><br />
-                <br/>
-                <NavLink to="/lessons" exact>
-                    <Button color={this.props.menu ? 'purple' : null} icon='file' size='massive' content='Lessons'/>
-                </NavLink><br />
-                <br/>
-                <NavLink to="/teacher-follow" exact>
-                    <Button color={this.props.menu ? 'purple' : null} icon='user' size='massive' content='See Following'/>
-                </NavLink>
-            </div>
-        );
-    }
+const TeacherHomeContainer = () => {
+    const menu = useSelector(state => state.app.menu)
+    return (  
+        <div>
+        <NavLink to="/classroom" exact>
+            <Button color={menu ? 'purple' : null} icon='book' size='massive' content='Classrooms'/>
+        </NavLink><br />
+        <br/>
+        <NavLink to="/lessons" exact>
+            <Button color={menu ? 'purple' : null} icon='file' size='massive' content='Lessons'/>
+        </NavLink><br />
+        <br/>
+        <NavLink to="/teacher-follow" exact>
+            <Button color={menu ? 'purple' : null} icon='user' size='massive' content='See Following'/>
+        </NavLink>
+    </div>
+    );
 }
 
-const mapStateToProps = state => ({
-    menu: state.app.menu
-})
-
-export default connect(mapStateToProps)(TeacherHomeContainer);
+export default TeacherHomeContainer;
